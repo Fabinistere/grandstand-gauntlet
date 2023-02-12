1f63a775024170dddc65c4198dc1ad3e6d6554a8
Wabtey <samurai.banzai@laposte.net>
2023-02-12 23:45:22 +0100

    impl Map Desert parallax
    with bevy-parallax

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
 use bevy::prelude::*;
 
+use bevy_parallax::{ParallaxCameraComponent, ParallaxPlugin};
+use constants::CLEAR;
+use locations::LocationsPlugin;
+
+pub mod constants;
+mod locations;
+
+#[rustfmt::skip]
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(Msaa { samples: 1 })
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    window: WindowDescriptor {
+                        title: "Grandstand Gauntlet".to_string(),
+                        ..default()
+                    },
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_plugin(ParallaxPlugin)
+        .add_plugin(LocationsPlugin)
+        .add_startup_system(spawn_camera)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let mut camera = Camera2dBundle::default();
+
+    camera.projection.scale = 0.2;
+
+    commands.spawn(camera).insert(ParallaxCameraComponent);
 }
