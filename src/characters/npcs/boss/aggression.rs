581834976ad8d074432e78d8c2a8374b4c36ac2d
Wabtey <wabtey@disroot.org>
2023-02-16 10:53:55 +0100

    Detection Structure
    a lonely skeleton

--- src/characters/npcs/boss/aggression.rs
+++ src/characters/npcs/boss/aggression.rs
@@ -1,7 +1,12 @@
+//! A big gouchi Boss
+
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 use crate::characters::{npcs::boss::Boss, player::Player, Invulnerable};
 
+/// DOC
+/// 
 /// Happens when:
 ///   - character::npcs:::boss_attack
 ///     - actions
@@ -12,6 +17,9 @@ pub struct BossAttackEvent {
     npc_entity: Entity,
     target_entity: Entity,
 }
+
+#[derive(Component)]
+pub struct BossSensor;
 
 /// When the player enters the sensor
 /// The boss start to attack them
@@ -28,10 +36,30 @@ pub struct BossAttackEvent {
 /// when tp/dying->soul shift the player become Invulnerable
 /// - If no player vulnerable, then remove the behavior
 pub fn boss_attack(
+    mut collision_events: EventReader<CollisionEvent>,
+    rapier_context: Res<RapierContext>,
+
+    boss_attack_sensor: Query<(Entity, &Parent), (With<Sensor>, With<BossSensor>)>,
+
     mut boss_query: Query<(&mut TextureAtlasSprite, &Transform), With<Boss>>,
     mut player_query: Query<(Entity, &Transform), (With<Player>, Without<Invulnerable>)>,
 ) {
     // TODO: Phase 1 - Sensor
+    for collision_event in collision_events.iter() {
+        let entity_1 = collision_event.entities().0;
+        let entity_2 = collision_event.entities().1;
+
+        if rapier_context.intersection_pair(entity_1, entity_2) == Some(true) {
+            match (boss_attack_sensor.get(e1), boss_attack_sensor.get(e2)) {
+                // (Err(e1), Err(e2)) => continue,
+                (Ok((attack_sensor, boss)), Err(_)) | (Err(_), Ok((attack_sensor, boss))) => {
+
+                }
+                _ => continue,
+
+            }
+        }
+    }
     // TODO: Phase 2 - Timer
     // TODO: Phase 3 - TP proof
 }
